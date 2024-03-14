d10cdfa3f874cb1f16b338e1a2b02db76c7d5c7b
georgipopov0 <georgipopov069@gmail.com>
2024-03-14 13:57:12 +0200

    Initial setup

---
+++ src/main/java/com/chadium/chadiumapi/ChadiumApiApplication.java
@@ -0,0 +1,13 @@
+package com.chadium.chadiumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChadiumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChadiumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chadium/chadiumapi/ChadiumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chadium.chadiumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChadiumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
